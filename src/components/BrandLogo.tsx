bbce37dfb3d82e5682d240876260112466f5a883
Admin Convemar <admin@convemar.com>
2026-08-14 14:48:17 -0600

    Remove map pin from O to avoid smudge look

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,10 +1,7 @@
-import { Star, MapPin } from 'lucide-react';
+import { Star } from 'lucide-react';
 
 export default function BrandLogo({ className = "", light = true }: { className?: string, light?: boolean }) {
   const textColor = light ? 'text-white' : 'text-brand-secondary';
-  
-  // El color del "recorte" debe ser igual al color de fondo para crear la ilusión de que la letra está hueca.
-  const cutoutColor = light ? 'text-brand-primary' : 'text-white';
   
   return (
     <div className={`flex flex-col items-center ${className}`}>
@@ -16,13 +13,7 @@ export default function BrandLogo({ className = "", light = true }: { className?
             <Star className="w-2.5 h-2.5 md:w-3 md:h-3 absolute -top-2 md:-top-2.5" fill="currentColor" />
             I
           </span>
-          LS
-          {/* La letra 'O' con el mapa (Pin) incrustado simulando un recorte */}
-          <span className="relative flex items-center justify-center mx-[1px]">
-             O
-             <MapPin className={`w-3 h-3 md:w-4 md:h-4 absolute ${cutoutColor} transform translate-y-[1px]`} fill="currentColor" />
-          </span>
-          N
+          LSON
         </div>
         <span className={`text-2xl md:text-3xl font-bold tracking-widest uppercase mt-1.5 ${textColor}`}>
           Pineda
